ccb5fd404c12bfff0bf1a4f152e31fe373ed12e6
emmanuelmailukyalo <90907246+emmanuelmailukyalo@users.noreply.github.com>
2022-03-15 17:34:47 +0300

    wacha tu cheki

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -17,3 +17,16 @@ void print_alphabet(void)
 
 	_putchar('\n');
 }
+
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
